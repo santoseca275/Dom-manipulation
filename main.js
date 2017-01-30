c3e761589c9662493efefe2990ac996af552700e
Erick C. Allen, Sr <eca275@gmail.com>
2017-01-30 18:15:28 -0500

    js edit

---
+++ main.js
@@ -0,0 +1,23 @@
+var resultsDiv = document.getElementById("answer");
+var textBox1 = document.getElementById("value-one");
+var textBox1 = document.getElementById("value-one");
+var button = document.getElementById("calculate-button");
+
+console.log(textBox1);
+
+function Calculator () {
+  var num1 = Number(textBox1.value);
+  var num2 = Number(textBox2.value);
+
+
+  function add (num1, num2) {
+    return num1 + num2;
+  }
+  
+  var total = ();
+
+}
+
+console.log(add(num1, num2));
+
+button.addEventListener('click', Calculator);
