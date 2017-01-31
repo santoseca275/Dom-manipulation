4881a85dd37cb1d3d981456c605f186ba976e9a6
Erick C. Allen, Sr <eca275@gmail.com>
2017-01-31 17:02:50 -0500

    Final Fixins Normal Mode

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var answerBox = document.getElementById("answer");
-
-button.addEventListener('click', Calculate);
-
-var textBox1 = document.getElementById("value-one");
-var textBox2 = document.getElementById("value-two");
 var button = document.getElementById("calculate-button");
 
-function Calculate (event) {
+
+button.addEventListener('click', calculate);
+
+var input1 = document.getElementById("value-one");
+var input2 = document.getElementById("value-two");
+var answerBox = document.getElementById("answer");
+
+
+function calculate (event) {
   event.preventDefault();
-  var total = Number(textBox1.value) + Number(textBox2.value);
+  var total = Number(input1.value) + Number(input2.value);
   answerBox.innerHTML = total;
 }
