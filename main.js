c0eaf0c1f6fb315dbf6cbc8bce51acee88af8e5b
Erick C. Allen, Sr <eca275@gmail.com>
2017-01-31 16:53:48 -0500

    Fixins

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-var resultsDiv = document.getElementById("answer");
+var answerBox = document.getElementById("answer");
+
+button.addEventListener('click', Calculate);
+
 var textBox1 = document.getElementById("value-one");
-var textBox1 = document.getElementById("value-one");
+var textBox2 = document.getElementById("value-two");
 var button = document.getElementById("calculate-button");
 
-console.log(textBox1);
-
-function Calculator () {
-  var num1 = Number(textBox1.value);
-  var num2 = Number(textBox2.value);
-
-
-  var add = function (num1, num2) {
-    return num1 + num2;
-  }
-
-  var total = (); {
-
-
-
-console.log(add(num1, num2));
+function Calculate (event) {
+  event.preventDefault();
+  var total = Number(textBox1.value) + Number(textBox2.value);
+  answerBox.innerHTML = total;
 }
-
-button.addEventListener('click', Calculator);
