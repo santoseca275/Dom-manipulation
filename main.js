39c70682de211793132fbde500a6a69cc94f81aa
Erick C. Allen, Sr <eca275@gmail.com>
2017-01-31 20:48:19 -0500

    Final Edits

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+var resultsDiv = document.getElementById("answer");
+var textBox1 = document.getElementById("value-one");
+// change textBox1 to textBox2 and fix the id
+var textBox2 = document.getElementById("value-two");
 var button = document.getElementById("calculate-button");
 
+console.log(textBox1);
 
-button.addEventListener('click', calculate);
+function Calculator () {
+  var num1 = Number(textBox1.value);
+  var num2 = Number(textBox2.value);
 
-var input1 = document.getElementById("value-one");
-var input2 = document.getElementById("value-two");
-var answerBox = document.getElementById("answer");
-
-
-function calculate (event) {
-  event.preventDefault();
-  var total = Number(input1.value) + Number(input2.value);
-  answerBox.innerHTML = total;
+  var answer = num1 + num2;
+  resultsDiv.innerHTML = answer;
 }
+
+button.addEventListener('click', Calculator);
